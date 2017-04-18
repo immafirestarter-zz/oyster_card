ba900adf6a48fb40368a9f692510a260389077ed
Bailey Kursar <bkursar@gmail.com>
2017-04-18 12:45:21 +0100

    Changed the error message to include the max balance

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,11 +1,14 @@
 class Oystercard
   attr_reader :balance
   MAXIMUM_BALANCE = 90
+
   def initialize
     @balance =0
   end
+
   def top_up(pounds)
-    fail "Limit Exceeded" if pounds + @balance > MAXIMUM_BALANCE
+    fail "Limit of £#{MAXIMUM_BALANCE} Exceeded" if pounds + @balance > MAXIMUM_BALANCE
     @balance = @balance + pounds
   end
+
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,13 +1,13 @@
 require './lib/oystercard'
 
 describe Oystercard do
+  MAXIMUM_BALANCE = 90
 
   it "has default value of 0" do
     expect(subject.balance).to eq(0)
   end
 
   context "topping up" do
-
     it { is_expected.to respond_to(:top_up).with(1).argument }
     it "can increase the balance" do
       expect { subject.top_up 1 }.to change { subject.balance }.by 1
@@ -15,8 +15,7 @@ describe Oystercard do
 
     it "fails when maximum capacity is exceeded" do
       subject.top_up(80)
-      expect { subject.top_up 20}.to raise_error "Limit Exceeded"
+      expect { subject.top_up 20 }.to raise_error "Limit of £#{MAXIMUM_BALANCE} Exceeded"
     end
   end
-
 end
